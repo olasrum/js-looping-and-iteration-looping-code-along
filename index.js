0995e05c7940d0938d84866262b9fe64888cce92
olasrum <mursalo@gmail.com>
2022-07-28 10:43:59 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let card = []
+    for (let i = 0; i < name.length; i++) {
+        card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    
+    return card;
+
+}
+
+console.log(writeCards(['Ada', 'Brendan', 'Ali'], 'birthday'));
+
+function countDown(n) {
+    let count = n;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+
+countDown(10);
